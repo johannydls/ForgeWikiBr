952348329b7dafe66c669f5f0f4951108cd1f297
johannydls <johanny.dls@gmail.com>
2017-07-07 17:30:03 -0300

    Conteudo recuperado do banco de dados e views adicionadas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,36 @@
 var app = angular.module('foe-wiki', ['ngRoute',
-									  'angularUtils.directives.dirPagination']);
+									  'angularUtils.directives.dirPagination',
+									  'firebase']);
 
-app.controller('pageController', ['$scope', function($scope) {
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'views/home.html'
+		})
+		.when('/unidadesMilitares', {
+			templateUrl: 'views/unidades_militares.html'
+		})
+		.otherwise({ 
+			redirectTo: '/home' 
+		});
+});
+
+app.controller('pageController', ['$scope','$firebaseArray', function($scope, $firebaseArray) {
+
+	var config = {
+		apiKey: "AIzaSyAJpKVSthdn_BD-E0jPdrIczzcJXGhKGp4",
+		authDomain: "api-foe.firebaseapp.com",
+		databaseURL: "https://api-foe.firebaseio.com",
+		projectId: "api-foe",
+		storageBucket: "api-foe.appspot.com",
+		messagingSenderId: "15272811973"
+	};
+
+	firebase.initializeApp(config);
+
+	var rootRef = firebase.database().ref().child('Eras').child('SemEra').child('UnidadeMilitar');
+
+	$scope.unidadesMilitaresSemEra = $firebaseArray(rootRef);
+
 	$scope.usuario = "Johanny LS";
 }]);
