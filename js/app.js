23fdbda3e85f7b8aa7daa6852ac0c6f60fde6b8e
johannydls <johanny.dls@gmail.com>
2017-07-13 18:50:19 -0300

    Implementacao do chat em andamento

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 var app = angular.module('foe-wiki', ['ngRoute',
 									  'angularUtils.directives.dirPagination',
 									  'firebase',
-									  'ngStorage']);
+									  'ngStorage',
+									  'luegg.directives']);
 
 app.config(function($routeProvider) {
 	$routeProvider
@@ -19,7 +20,7 @@ app.config(function($routeProvider) {
 			templateUrl: 'views/login.html',
 			controller: 'loginController'
 		})
-		.when('/gerenciador', {
+		.when('/admin', {
 			title: 'Gerenciador',
 			templateUrl: 'views/gerenciador.html',
 			controller: 'gerenciadorController'
@@ -31,6 +32,11 @@ app.config(function($routeProvider) {
 		.when('/edificios', {
 			title: 'Edifícios',
 			templateUrl: 'views/edificios.html'
+		})
+		.when('/chat', {
+			title: 'Chat',
+			templateUrl: 'views/chat.html',
+			controller: 'chatController'
 		})
 		.otherwise({ 
 			redirectTo: '/home' 
@@ -106,6 +112,52 @@ app.controller('loginController', ['$scope', 'bd_app', function($scope, bd_app) 
 
 }]);
 
+app.controller('chatController', ['$scope','bd_app', function ($scope, bd_app, $sessionStorage, $rootScope) {
+
+	$scope.usuarios = bd_app.getUsuarios();
+	$scope.avatars = bd_app.getAvatars();
+	$scope.mensagens = bd_app.getMensagens();
+
+	$scope.enviarMsg = function() {
+		bd_app.enviarMensagem(
+			$scope.corpoMsg,
+			"NinjaX",
+			"Admin"
+		);
+
+		$scope.corpoMsg = '';
+	}
+
+	$scope.getAvatarDonoMsg = function(donoId) {
+		
+		for (var i = 0; i < $scope.usuarios.length; i++) {
+			if ($scope.usuarios[i].$id == donoId) {
+				return $scope.usuarios[i].Avatar;
+			}
+		}
+	}
+
+	$scope.getDonoMsgAdmin = function(donoId) {
+		for (var i = 0; i < $scope.usuarios.length; i++) {
+			if ($scope.usuarios[i].$id == donoId) {
+				return $scope.usuarios[i].Admin;
+			}
+		}
+	}
+
+	$scope.getDonoMsgMundo = function(donoId) {
+		for (var i = 0; i < $scope.usuarios.length; i++) {
+			if ($scope.usuarios[i].$id == donoId) {
+				return $scope.usuarios[i].Mundo;
+			}
+		}
+	}
+
+	$scope.glued = true;
+	
+}]);
+
+
 app.service('bd_app', ['$rootScope','$firebaseArray', '$location', '$sessionStorage', 
 				function ($rootScope, $firebaseArray, $location, $sessionStorage) {
 	
@@ -121,22 +173,55 @@ app.service('bd_app', ['$rootScope','$firebaseArray', '$location', '$sessionStor
 	firebase.initializeApp(config);
 
 	var rootRef = firebase.database().ref().child('Recursos');
-	
 	var rootAuth = firebase.database().ref().child('Admin');
+	var rootChat = firebase.database().ref().child('Chat');
 
 	this.unidadesMilitares = $firebaseArray(rootRef.child('UnidadeMilitar'));
 	this.usuarioAdmin = $firebaseArray(rootAuth);
+	this.avatars = $firebaseArray(rootChat.child('Avatars'));
+	this.usuarios = $firebaseArray(rootChat.child('Usuarios'));
+	this.mensagens = $firebaseArray(rootChat.child('Mensagens'));
 	
 	this.getUnidadesMilitares = function() {
 		return this.unidadesMilitares;
 	}
 
-	this.calculaBonus = function(valor, bonus) {
-		return Math.round(valor + ((bonus/100) * valor));
+	this.getAvatars = function() {
+		return this.avatars;
 	}
 
-	this.getNumUnidades = function() {
-		return this.unidadesMilitares.length;
+	this.getUsuarios = function() {
+		return this.usuarios;
+	}
+
+	this.getMensagens = function() {
+		return this.mensagens;
+	}
+
+	this.criarUsuario = function(avatar, mundo, nick) {
+
+		var newUserId = rootChat.child('Usuarios').push({
+			Admin: false,
+			Avatar: avatar,
+			Mundo: mundo,
+			Nick: nick
+		}).key;
+
+		$rootScope.usuarioChatId = newUserId;
+		$sessionStorage.usuarioChatId = newUserId;
+	}
+
+	this.enviarMensagem = function(corpo, dono, id) {
+		rootChat.child('Mensagens').push({
+			Corpo: corpo,
+			Dono: dono,
+			Id: id
+		});
+	}
+	
+
+	this.calculaBonus = function(valor, bonus) {
+		return Math.round(valor + ((bonus/100) * valor));
 	}
 
 	this.addUnidade = function(nome, tipo, era, atk, def, alcance, movimento, imagem, tipoIcone) {
@@ -174,7 +259,19 @@ app.service('bd_app', ['$rootScope','$firebaseArray', '$location', '$sessionStor
 		$location.path('/home');
 	}
 
+	this.logoutChat = function() {
+		$rootScope.usuarioChat = null;
+		$sessionStorage.usuarioChat = null;
+		$location.path('/chat');
+	}
+
 }]);
+
+app.filter('reverse', function() {
+  return function(items) {
+    return items.slice().reverse();
+  };
+});
 
 app.run(function ($rootScope, $location, $sessionStorage) {
 
